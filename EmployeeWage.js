ca47d5f27be218e1ecae904de7d1dc070ddce9b1
Ganesh Gavhad <ganeshgavhad9@gmail.com>
2021-08-19 22:55:38 +0530

    Refactor : UC2 code to write function for daily work Hrs

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -14,26 +14,27 @@
     }
 }
 
-//UC 2 using switch case to calculate employee wage
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOURS = 20;
 
-{
-  let empHr = 0;
-  let empCheck = Math.floor(Math.random()*10)%3;
+//UC3 Refactored code to write function for daily work hrs.
+
+function getWorkingHours(empCheck){
   switch(empCheck){
     case IS_PART_TIME:
-          empHr = PART_TIME_HOURS;
-          break;
+      return PART_TIME_HOURS;
     case IS_FULL_TIME:
-          empHr = FULL_TIME_HOURS;
-          break;
+      return FULL_TIME_HOURS;
     default:
-          empHr = 0;           
-  }
-  let empWage =empHr*WAGE_PER_HOURS;
-  console.log("UC2 - Emp Wage : "+empWage)
+      return 0;    
+  
+    }
 }
+let empHr = 0;
+let empCheck = Math.floor(Math.random()*10)%3;
+empHr = getWorkingHours(empCheck);
+let empWage = empHr * WAGE_PER_HOURS;
+console.log("Emp Wage: "+ empWage);
